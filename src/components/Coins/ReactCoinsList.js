c945a27039ee9fda25dc939562ad57c91ad7dc1c
Daniel Corcoran <danielc.92@outlook.com>
2019-07-25 17:27:28 +1000

    Adjusted coins list into a table

--- src/components/Coins/ReactCoinsList.js
+++ src/components/Coins/ReactCoinsList.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
-import { Layout, Table, Typography, Button, Card, List } from 'antd';
+import { Layout, Table, Typography, Button, Tag } from 'antd';
 import { contentStyle } from '../../styles';
 const { Content } = Layout;
 const { Title, Paragraph } = Typography;
@@ -11,29 +11,54 @@ class ReactCoinsList extends Component {
 
     state = {
         coinsList: [],
-        loaded: true
+        loading: true
     }
 
     componentDidMount () {
         axios.get('https://api.coingecko.com/api/v3/coins/list')
         .then(res => {
             this.setState({coinsList: res.data}, 
-                () => {this.setState({loaded: !this.state.loaded })})
+                () => {this.setState({loading: !this.state.loading })})
         })
         .catch(error => console.error(error))
     }
 
     render() {
-
+        const { coinsList, loading } = this.state;
         const columns=[
-            {}
+            {
+                title:'Id',
+                dataIndex:'id',
+                key:'id'
+            },
+            {
+                title:'Symbol',
+                dataIndex:'symbol',
+                key:'symbol'
+            },
+            {
+                title:'Name',
+                dataIndex:'name',
+                key:'name',
+                render: item => <Tag color="purple">{item}</Tag>
+            },
+            {
+                title:'View Details',
+                dataIndex:'symbol',
+                key:'symbol',
+                render: item => (<Button type="primary"><Link to="/empty">View</Link></Button>)
+            }
         ];
         return (
             <Layout style={{ padding: '1rem' }}>
                 <Content style={contentStyle}>
                     <Title level={2}>Coins List</Title>
                     <Paragraph>This page lists all available coins...</Paragraph>
-                    <Table dataSource={this.state.coinsList} columns={columns}/>
+                    <Table 
+                    bordered={true}
+                    loading={loading} 
+                    dataSource={coinsList} 
+                    columns={columns}/>
                 </Content>
             </Layout>
         )
