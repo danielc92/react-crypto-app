1fffc914a549fd627e5ad1af2c07d64378351761
Daniel Corcoran <danielc.92@outlook.com>
2019-07-25 10:52:24 +1000

    Added some placeholders for submenus, to be expanded later on

--- src/components/Layout/ReactSider.js
+++ src/components/Layout/ReactSider.js
@@ -11,51 +11,44 @@ class ReactSider extends Component {
                 <Sider width={200} style={{ background: '#fff' }}>
                         <Menu
                         mode="inline"
-                        defaultSelectedKeys={['1']}
-                        defaultOpenKeys={['sub1']}
+                        defaultSelectedKeys={['menu-coins']}
+                        defaultOpenKeys={['menu-coins', 'menu-events', 'menu-global']}
                         style={{ height: '100%', borderRight: 0 }}
                         >
                         <SubMenu
-                            key="sub1"
+                            key="menu-coins"
                             title={
                             <span>
-                                <Icon type="user" />
-                                subnav 1
+                                <Icon type="dollar" />Subnav
                             </span>
                             }
                         >
-                            <Menu.Item key="1">option1</Menu.Item>
-                            <Menu.Item key="2">option2</Menu.Item>
-                            <Menu.Item key="3">option3</Menu.Item>
-                            <Menu.Item key="4">option4</Menu.Item>
+                            <Menu.Item key="coin-list">List View</Menu.Item>
+                            <Menu.Item key="coin-market">Markets</Menu.Item>
+                            <Menu.Item key="coin-detail">Detail View</Menu.Item>
                         </SubMenu>
                         <SubMenu
-                            key="sub2"
+                            key="menu-events"
                             title={
                             <span>
-                                <Icon type="laptop" />
-                                subnav 2
+                                <Icon type="team" />Events
                             </span>
                             }
                         >
-                            <Menu.Item key="5">option5</Menu.Item>
-                            <Menu.Item key="6">option6</Menu.Item>
-                            <Menu.Item key="7">option7</Menu.Item>
-                            <Menu.Item key="8">option8</Menu.Item>
+                            <Menu.Item key="events-latest">Latest Events</Menu.Item>
+                            <Menu.Item key="events-all">All Events</Menu.Item>
+                            <Menu.Item key="events-countries">Countries</Menu.Item>
                         </SubMenu>
                         <SubMenu
-                            key="sub3"
+                            key="menu-global"
                             title={
                             <span>
-                                <Icon type="notification" />
-                                subnav 3
+                                <Icon type="global" />
+                                Global
                             </span>
                             }
                         >
-                            <Menu.Item key="9">option9</Menu.Item>
-                            <Menu.Item key="10">option10</Menu.Item>
-                            <Menu.Item key="11">option11</Menu.Item>
-                            <Menu.Item key="12">option12</Menu.Item>
+                            <Menu.Item key="9">Main</Menu.Item>
                         </SubMenu>
                         </Menu>
                     </Sider>
