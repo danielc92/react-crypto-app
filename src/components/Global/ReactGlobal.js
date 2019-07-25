b721eea7d3f34c63414281dba48818464b433678
Daniel Corcoran <danielc.92@outlook.com>
2019-07-25 17:16:31 +1000

    Converted global cards into Statistic components from ANTD

--- src/components/Global/ReactGlobal.js
+++ src/components/Global/ReactGlobal.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import axios from 'axios';
-import { Col, Skeleton, Card, Row, Layout, Typography } from 'antd';
+import { Col, Skeleton, Statistic, Card, Row, Layout, Typography } from 'antd';
 import { contentStyle } from '../../styles';
 const { Meta } = Card;
 const { Content } = Layout;
@@ -18,7 +18,8 @@ export default class ReactGlobal extends Component {
             this.setState({ loading: !this.state.loading })))
     }
     render() {
-        const cardStyle = {marginBottom: '1rem'};
+        const cardStyle = {height: '100%'};
+        const colStyle = {marginBottom: '1rem'};
         const { loading,
             active_cryptocurrencies, 
             market_cap_change_percentage_24h_usd,
@@ -28,74 +29,50 @@ export default class ReactGlobal extends Component {
             updated_at,
             markets} = this.state.data
         return (
-            <Layout style={{ padding: '1rem' }}>
-                <Content style={contentStyle}>
-                    <Title level={2}>Global Data</Title>
-                    <Paragraph>This page lists global data. Prices were last updated at {}</Paragraph>
-                    <Row gutter={16} style={{textAlign: 'center'}}>
+        <Layout style={{ padding: '1rem' }}>
+            <Content style={contentStyle}>
+                <Title level={2}>Global Data</Title>
+                <Paragraph>This page lists global data. Prices were last updated at {}</Paragraph>
+                <Row gutter={16} style={{textAlign: 'center'}} type="flex">
 
-                        <Col xs={24} sm={24} md={8} lg={8} xl={8}>
-                            <Card style={cardStyle}>
-                                <Skeleton loading={loading}>
-                                    <Meta
-                                    title="Markets"
-                                    description={markets}/>
-                                </Skeleton>
-                            </Card>
-                        </Col>
+                    <Col xs={24} sm={24} md={8} lg={8} xl={8} style={colStyle}>
+                    <Card style={cardStyle}>
+                            <Statistic title="Markets" value={markets}/>
+                        </Card>
+                    </Col>
 
-                        <Col xs={24} sm={24} md={8} lg={8} xl={8}>
-                            <Card style={cardStyle}>
-                                <Skeleton loading={loading}>
-                                    <Meta
-                                    title="Market Cap Change Percent (24hr USD)"
-                                    description={market_cap_change_percentage_24h_usd}/>
-                                </Skeleton>
-                            </Card>
-                        </Col>
-
-                        <Col xs={24} sm={24} md={8} lg={8} xl={8}>
+                    <Col xs={24} sm={24} md={8} lg={8} xl={8} style={colStyle}>
                         <Card style={cardStyle}>
-                            <Skeleton loading={loading}>
-                                <Meta
-                                title="Ended Icos"
-                                description={ended_icos}/>
-                            </Skeleton>
-                            </Card>
-                        </Col>
+                            <Statistic suffix='%' precision={2} title="Market Cap Change Percent (24hr USD)" value={market_cap_change_percentage_24h_usd}/>
+                        </Card>
+                    </Col>
 
-                        <Col xs={24} sm={24} md={8} lg={8} xl={8}>
+                    <Col xs={24} sm={24} md={8} lg={8} xl={8} style={colStyle}>
                         <Card style={cardStyle}>
-                            <Skeleton loading={loading}>
-                                    <Meta
-                                    title="Active Cryptocurrencies"
-                                    description={active_cryptocurrencies}/>
-                                </Skeleton>
-                            </Card>
-                        </Col>
+                            <Statistic title="Ended Icos" value={ended_icos} />
+                        </Card>
+                    </Col>
 
-                        <Col xs={24} sm={24} md={8} lg={8} xl={8}>
-                            <Card style={cardStyle}>
-                                <Skeleton loading={loading}>
-                                    <Meta
-                                    title="Ongoing Icos"
-                                    description={ongoing_icos}/>
-                                </Skeleton>
-                            </Card>
-                        </Col>
+                    <Col xs={24} sm={24} md={8} lg={8} xl={8} style={colStyle}>
+                        <Card style={cardStyle}>
+                            <Statistic title="Active Cryptocurrencies" value={active_cryptocurrencies}/>
+                        </Card>
+                    </Col>
 
-                        <Col xs={24} sm={24} md={8} lg={8} xl={8}>
-                            <Card style={cardStyle}>
-                                <Skeleton loading={loading}>
-                                    <Meta
-                                    title="Upcoming Icos"
-                                    description={upcoming_icos}/>
-                                </Skeleton>
-                            </Card>
-                        </Col>
-                    </Row>
-                </Content>
-            </Layout>
+                    <Col xs={24} sm={24} md={8} lg={8} xl={8} style={colStyle}>
+                    <Card style={cardStyle}>
+                            <Statistic title="Ongoing Initial Coin Offerings (ICOS)" value={ongoing_icos}/>
+                        </Card>
+                    </Col>
+
+                    <Col xs={24} sm={24} md={8} lg={8} xl={8} style={colStyle}>
+                        <Card style={cardStyle}>
+                            <Statistic title="Upcoming Initial Coin Offerings (ICOS)" value={upcoming_icos}/>
+                        </Card>
+                    </Col>
+                </Row>
+            </Content>
+        </Layout>
         )
     }
 }
