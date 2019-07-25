87aca8cc4e2e28e3d1ed17f8b7aac25b77c08596
Daniel Corcoran <danielc.92@outlook.com>
2019-07-25 16:34:09 +1000

    Updates

---
+++ precache-manifest.0eea40dc78238bf7adb640088baf2635.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b946029e83c59cb3e99cec6df075e05",
+    "url": "/react-crypto-app/index.html"
+  },
+  {
+    "revision": "993a44d0151482892756",
+    "url": "/react-crypto-app/static/css/2.cb759328.chunk.css"
+  },
+  {
+    "revision": "0bc4b8eb4589f5ece23c",
+    "url": "/react-crypto-app/static/css/main.7dc16004.chunk.css"
+  },
+  {
+    "revision": "993a44d0151482892756",
+    "url": "/react-crypto-app/static/js/2.e50bcfc9.chunk.js"
+  },
+  {
+    "revision": "0bc4b8eb4589f5ece23c",
+    "url": "/react-crypto-app/static/js/main.491618b0.chunk.js"
+  },
+  {
+    "revision": "481feaf89526f90b4de5",
+    "url": "/react-crypto-app/static/js/runtime~main.e61d5480.js"
+  }
+]);
