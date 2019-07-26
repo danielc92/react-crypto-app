d78a98fd491980342708fe2ec8dc6afedb4ded88
Daniel Corcoran <danielc.92@outlook.com>
2019-07-26 11:46:58 +1000

    Finished working on home page svgs

--- src/components/General/ReactHome.js
+++ src/components/General/ReactHome.js
@@ -1,18 +1,42 @@
 import React, { Component } from 'react';
-import { Layout, Typography } from 'antd';
+import { Layout, Typography, Card, Row, Col } from 'antd';
 import { contentStyle } from '../../styles';
-const { Content } = Layout;
-const { Title, Paragraph } = Typography;
+import svg1 from '../../FlatIcon/001-bitcoin.svg';
+import svg2 from '../../FlatIcon/002-bitcoin-1.svg';
+import svg3 from '../../FlatIcon/003-bitcoin-2.svg';
+import { colStyle } from '../../styles';
 
+const { Content } = Layout;
+const { Meta } = Card;
+const { Title, Paragraph } = Typography;
+const svgs = [svg1, svg2, svg3];
 
 class ReactHome extends Component {
-
+    
     render() {
         return (
             <Layout style={{ padding: '1rem' }}>
                 <Content  className="text-focus-in" style={contentStyle}>
                     <Title level={2}>Home Page</Title>
                     <Paragraph>Consequat fugiat exercitation cillum proident aliquip irure non. Ad ad qui labore do nulla. Est pariatur culpa laborum labore aliqua incididunt. Sunt do ex aliqua irure magna adipisicing sint exercitation pariatur adipisicing. Tempor eiusmod minim officia nostrud eiusmod sunt veniam ipsum cillum enim ut consequat. Fugiat eiusmod ut sit ea commodo do eiusmod ex deserunt.</Paragraph>
+
+                    <Title level={2}>Cards</Title>
+                    <Row gutter={16} style={{textAlign: 'center'}} type="flex">
+                        {
+                            svgs.map(svg => (
+                                <Col xs={24} sm={24} md={8} lg={8} xl={8} style={colStyle}>
+                                    <Card 
+                                    style={{border: 'none'}}
+                                    cover={<img style={{padding:'2rem', maxWidth: '200px', margin: 'auto'}} src={svg}/>} 
+                                    >
+                                        <Meta 
+                                        title="svg-image" 
+                                        description="Id mollit dolore tempor nulla ad aliqua irure aute reprehenderit commodo officia." />
+                                    </Card>
+                                </Col>
+                            ))
+                        }
+                    </Row>
                 </Content>
             </Layout>
         )
