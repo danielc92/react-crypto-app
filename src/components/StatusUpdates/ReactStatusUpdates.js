2b34ad388e925764acec85178911a3f8cd401f4d
Daniel Corcoran <danielc.92@outlook.com>
2019-07-26 12:02:32 +1000

    Added ReactStatusUpdates

---
+++ src/components/StatusUpdates/ReactStatusUpdates.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+
+export default class ReactStatusUpdates extends Component {
+    
+    componentDidMount() {
+        const url = 'https://api.coingecko.com/api/v3/status_updates'
+    }
+
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
