319fb2e7a959efa14626eb1e952fab6cacc546ce
Daniel Corcoran <danielc.92@outlook.com>
2019-07-27 19:06:33 +1000

    Edited features on home page

--- src/components/General/ReactHome.js
+++ src/components/General/ReactHome.js
@@ -3,13 +3,22 @@ import { Layout, Typography, Card, Row, Col } from 'antd';
 import { contentStyle } from '../../styles';
 import svg1 from '../../FlatIcon/001-bitcoin.svg';
 import svg2 from '../../FlatIcon/002-bitcoin-1.svg';
-import svg3 from '../../FlatIcon/003-bitcoin-2.svg';
+import svgExchangeRate from '../../FlatIcon/004-exchange.svg';
+import svgChat from '../../FlatIcon/001-chat.svg';
+import svgMobile from '../../FlatIcon/003-smartphone.svg';
+import svgUpdate from '../../FlatIcon/005-idea.svg';
 import { colStyle } from '../../styles';
 
 const { Content } = Layout;
 const { Meta } = Card;
 const { Title, Paragraph } = Typography;
-const svgs = [svg1, svg2, svg3];
+
+const features = [{svg: svg1, title:'Statistics', description: 'Stay informed with the latest up to date information pertaining to your favourite cryptocurrencies.'},
+{svg: svgChat, title:'Community', description: 'Be informed about upcoming cryptocurrency events in your area.'},
+{svg: svgExchangeRate, title:'Exchange Rates', description: 'View up to date exchange rates for your favourite cryptocurrencies.'},
+{svg: svgMobile, title:'Mobile Friendly', description: 'This site aims to be mobile friendly. View website on any device you like.'},
+{svg: svgUpdate, title:'Updates', description: 'App is continuously updated with'}
+];
 
 class ReactHome extends Component {
     
@@ -17,26 +26,28 @@ class ReactHome extends Component {
         return (
             <Layout style={{ padding: '1rem' }}>
                 <Content  className="text-focus-in" style={contentStyle}>
-                    <Title level={2}>Home Page</Title>
-                    <Paragraph>Consequat fugiat exercitation cillum proident aliquip irure non. Ad ad qui labore do nulla. Est pariatur culpa laborum labore aliqua incididunt. Sunt do ex aliqua irure magna adipisicing sint exercitation pariatur adipisicing. Tempor eiusmod minim officia nostrud eiusmod sunt veniam ipsum cillum enim ut consequat. Fugiat eiusmod ut sit ea commodo do eiusmod ex deserunt.</Paragraph>
+                    <Title level={2}>Cryto</Title>
+                    <Paragraph>Bringing you cryptocurrency related data in a user friendly, digestible dashboard platform.</Paragraph>
 
-                    <Title level={2}>Cards</Title>
+                    <Title level={2}>Features</Title>
+                    
                     <Row gutter={16} style={{textAlign: 'center'}} type="flex">
                         {
-                            svgs.map((svg, index) => (
+                            features.map((item, index) => (
                                 <Col key={index} xs={24} sm={24} md={8} lg={8} xl={8} style={colStyle}>
                                     <Card 
                                     style={{border: 'none'}}
-                                    cover={<img alt="Home page crypto svg" style={{padding:'2rem', maxWidth: '200px', margin: 'auto'}} src={svg}/>} 
+                                    cover={<img alt="Home page crypto svg" style={{padding:'2rem', maxWidth: '200px', margin: 'auto'}} src={item.svg}/>} 
                                     >
                                         <Meta 
-                                        title="svg-image" 
-                                        description="Id mollit dolore tempor nulla ad aliqua irure aute reprehenderit commodo officia." />
+                                        title={item.title} 
+                                        description={item.description} />
                                     </Card>
                                 </Col>
                             ))
                         }
                     </Row>
+                    <Paragraph>Icons provided by flaticon.</Paragraph>
                 </Content>
             </Layout>
         )
