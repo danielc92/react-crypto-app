04837c3816e51b48a1ad97886c8082e581fbcf4d
Daniel Corcoran <danielc.92@outlook.com>
2019-07-27 10:29:41 +1000

    Added challenges to about page

--- src/components/General/ReactAbout.js
+++ src/components/General/ReactAbout.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Layout, Button, Tag, Typography, Table } from 'antd';
+import { Layout, List, Tag, Typography, Table } from 'antd';
 import { contentStyle } from '../../styles';
 import geckoImage from '../../Branding/CoinGecko.png';
 
@@ -10,12 +10,7 @@ const { Title, Paragraph } = Typography;
 class ReactAbout extends Component {
 
     render() {
-        // "antd": "^3.20.6",
-        // "gh-pages": "^2.0.1",
-        // "react": "^16.8.6",
-        // "react-dom": "^16.8.6",
-        // "react-router-dom": "^5.0.1",
-        // "react-scripts": "3.0.1"
+
         const packages = [
             {
                 name: 'antd',
@@ -51,6 +46,11 @@ class ReactAbout extends Component {
                 name: 'react-chartjs-2',
                 version: '^2.7.6',
                 purpose: ''
+            },
+            {
+                name: 'axios',
+                version :'^0.19.0',
+                purpose: ''
             }
             ];
 
@@ -66,32 +66,54 @@ class ReactAbout extends Component {
                 key: 'version',
                 render: item => <Tag color="green">{item}</Tag>
             }];
-        
+            
+            const challenges = [
+                'Organizing large amounts of data from third party API.'
+                ,'Complex routing within nested components.'
+                ,'Restructuring data from API calls into a suitable format for UI components.'
+                ,'Creating maintainable structure for components and resusable configurations.'
+                ,'Learning to use a brand new UI framework (Ant Design UI).'
+            ]
+
         return (
             <Layout style={{ padding: '1rem' }}>
                 <Content className="text-focus-in" style={{...contentStyle}}>
                     <Title level={2}>About Page</Title>
                     <Paragraph>Consolidating RESTFUL calls from the CoinGecko API into a React application. I'll be using v3 of the API (the most recent version at the time of starting this project) This app will be using ant design for UI. </Paragraph>
-                    <Title level={4}>Purpose</Title>
+                    
+                    
+                    <Title level={3}>Purpose</Title>
                     <Paragraph>This project was built in order to further learning and development of React applications, especially those involving connecting to third party APIs.</Paragraph>
-                    <Title level={4}>Packages used in this project</Title>
+                    
+                    <Title level={3}>Data source</Title>
+                    <Paragraph>All the data which this application ingests and displays is processed from the CoinGecko API. A free and publicly available cryptocurrency api. Click below to visit CoinGecko official website.</Paragraph>
+                    
+                        <a href="https://www.coingecko.com/en/api#">
+                        <img 
+                        style={{border: '1px solid #f3f3f3', maxWidth: '400px'}} 
+                        src={geckoImage}>
+                        </img>
+                    </a>
+                    <Paragraph/>
+
+                    <Title level={3}>Challenges</Title>
+                    <Paragraph>Building this application posed some challenges;</Paragraph>
+                    <List
+                    style={{marginBottom: '2rem'}}
+                    size="medium"
+                    bordered
+                    dataSource={challenges}
+                    renderItem={c => <List.Item>{c}</List.Item>}
+                    />
+
+                    <Title level={3}>Packages used in this project</Title>
                     <Paragraph>The following packages were installed via <strong>npm</strong></Paragraph>
                     
                     <Table
                     pagination={false} 
                     dataSource={packages} 
                     columns={columns}></Table>
-
-                    <Paragraph></Paragraph>
-                    <Title level={4}>Data source</Title>
-                    <Paragraph>All the data which this application ingests and displays is processed from the CoinGecko API. A free and publicly available cryptocurrency api. Click below to visit CoinGecko official website.</Paragraph>
-                    <a href="https://www.coingecko.com/en/api#">
-                        <img 
-                        style={{border: '1px solid #f3f3f3'}} 
-                        width="550" 
-                        src={geckoImage}>
-                        </img>
-                    </a>
+                    
                 </Content>
             </Layout>
         )
