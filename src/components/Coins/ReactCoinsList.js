c1f16675235ea65e4e65430a4deb7d02cbc4062f
Daniel Corcoran <danielc.92@outlook.com>
2019-07-27 11:02:33 +1000

    Added table filter from ANTD docs for coin listing

--- src/components/Coins/ReactCoinsList.js
+++ src/components/Coins/ReactCoinsList.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import Highlighter from 'react-highlight-words';
 import axios from 'axios';
-import { Layout, Table, Typography, Button, Tag } from 'antd';
+import { Layout, Table, Typography, Button, Tag, Input, Icon } from 'antd';
 import { contentStyle } from '../../styles';
 const { Content } = Layout;
 const { Title, Paragraph } = Typography;
@@ -10,9 +11,70 @@ const { Title, Paragraph } = Typography;
 class ReactCoinsList extends Component {
 
     state = {
+        searchText: '',
         coinsList: [],
         loading: true
     }
+
+    getColumnSearchProps = dataIndex => ({
+        filterDropdown: ({ setSelectedKeys, selectedKeys, confirm, clearFilters }) => (
+            <div style={{ padding: 8 }}>
+            <Input
+                ref={node => {
+                this.searchInput = node;
+                }}
+                placeholder={`Search ${dataIndex}`}
+                value={selectedKeys[0]}
+                onChange={e => setSelectedKeys(e.target.value ? [e.target.value] : [])}
+                onPressEnter={() => this.handleSearch(selectedKeys, confirm)}
+                style={{ width: 188, marginBottom: 8, display: 'block' }}
+            />
+            <Button
+                type="primary"
+                onClick={() => this.handleSearch(selectedKeys, confirm)}
+                icon="search"
+                size="small"
+                style={{ width: 90, marginRight: 8 }}
+            >
+                Search
+            </Button>
+            <Button onClick={() => this.handleReset(clearFilters)} size="small" style={{ width: 90 }}>
+                Reset
+            </Button>
+            </div>
+        ),
+        filterIcon: filtered => (
+            <Icon type="search" style={{ color: filtered ? '#1890ff' : undefined }} />
+        ),
+        onFilter: (value, record) =>
+            record[dataIndex]
+            .toString()
+            .toLowerCase()
+            .includes(value.toLowerCase()),
+        onFilterDropdownVisibleChange: visible => {
+            if (visible) {
+            setTimeout(() => this.searchInput.select());
+            }
+        },
+        render: text => (
+            <Highlighter
+            highlightStyle={{ backgroundColor: '#ffc069', padding: 0 }}
+            searchWords={[this.state.searchText]}
+            autoEscape
+            textToHighlight={text.toString()}
+            />
+        ),
+    });
+
+    handleSearch = (selectedKeys, confirm) => {
+        confirm();
+        this.setState({ searchText: selectedKeys[0] });
+    };
+    
+    handleReset = clearFilters => {
+        clearFilters();
+        this.setState({ searchText: '' });
+    };
 
     componentDidMount () {
         axios.get('https://api.coingecko.com/api/v3/coins/list')
@@ -29,17 +91,20 @@ class ReactCoinsList extends Component {
             {
                 title:'Id',
                 dataIndex:'id',
-                key:'id'
+                key:'id',
+                ...this.getColumnSearchProps('id')
             },
             {
                 title:'Symbol',
                 dataIndex:'symbol',
-                key:'symbol'
+                key:'symbol',
+                ...this.getColumnSearchProps('symbol')
             },
             {
                 title:'Name',
                 dataIndex:'name',
                 key:'name',
+                ...this.getColumnSearchProps('name'),
                 render: item => <Tag color="purple">{item}</Tag>
             },
             {
