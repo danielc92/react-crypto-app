691dfdbfa4e8d7eadc56e3425db75e92a088d0a2
Daniel Corcoran <danielc.92@outlook.com>
2019-07-29 10:41:22 +1000

    Added status updates reducer

--- src/redux_reducers/index.js
+++ src/redux_reducers/index.js
@@ -12,7 +12,18 @@ const eventReducer = (state=[], action) => {
     }
 }
 
+const statusUpdatesReducer = (state=[], action) => {
+    const { type, payload } = action;
+
+    switch(type) {
+        case 'GET_STATUS_UPDATES':
+            return payload.data
+        default: 
+            return state
+    }
+}
 
 export const rootReducer = combineReducers({
-    events: eventReducer
+    events: eventReducer,
+    status_updates: statusUpdatesReducer
 })
