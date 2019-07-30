019175debd4128179ad6c80913119a44ede6ab07
Daniel Corcoran <danielc.92@outlook.com>
2019-07-30 11:35:40 +1000

    Added community and developer stat components

--- src/components/Coins/ReactCoinsDetail.js
+++ src/components/Coins/ReactCoinsDetail.js
@@ -143,24 +143,8 @@ class ReactCoinsDetail extends Component {
                                         ))
                                     }
                                 </Row>
-
-                                <Title level={3} style={titleStyle}>
-                                    Community Stats
-                                </Title>
-
-                                <Row gutter={16} style={{textAlign: 'center'}} type="flex">
-                                    { Object.entries(community_data).map(item => {
-                                        if (typeof item[1] !== 'object') {
-                                            return (
-                                            <Col xs={24} sm={24} md={12} lg={8} xl={8} style={colStyle}>
-                                                <Card style={cardStyle}>
-                                                    <Statistic precision={1} title={item[0]} value={item[1]}/>
-                                                </Card>
-                                            </Col>
-                                            )
-                                        }
-                                    })}
-                                </Row>
+                                
+                                
                                 
 
                                 <Title level={3} style={titleStyle}>
